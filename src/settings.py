51633be80ad9f5df0596094d01f6f363f1e2a8b0
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-09-07 19:36:48 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "CODEBRIM"
 PROJECT_NAME_FULL: str = "CODEBRIM: COncrete DEfect BRidge IMage Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
